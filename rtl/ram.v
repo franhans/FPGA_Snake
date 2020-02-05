ca83218187b6f295f97aa4f84c31d28a636f08ce
Fran <franbjalon@gmail.com>
2020-02-05 11:34:30 +0100

    RAM is being added to print the screen using sprites.

---
+++ rtl/ram.v
@@ -0,0 +1,21 @@
+module sram #(parameter ADDR_WIDTH=10, DATA_WIDTH=2, DEPTH=640) (
+    input wire i_clk,
+    input wire [ADDR_WIDTH-1:0] i_addr, 
+    input wire i_write,
+    input wire [DATA_WIDTH-1:0] i_data,
+    output reg [DATA_WIDTH-1:0] o_data 
+    );
+
+    reg [DATA_WIDTH-1:0] memory_array [0:DEPTH-1]; 
+
+
+    always @ (posedge i_clk)
+    begin
+        if(i_write) begin
+            memory_array[i_addr] <= i_data;
+        end
+        else begin
+            o_data <= memory_array[i_addr];
+        end     
+    end
+endmodule
