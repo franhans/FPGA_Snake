b3b899f345fa484f61da1d45363b09b72bc94682
Fran <franbjalon@gmail.com>
2020-02-07 15:26:08 +0100

    The head moves properly but the tail doesn't.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERILOG_FILES = rtl/Snake.v rtl/top.v  rtl/VGA/VgaSyncGen.v  rtl/UART/rx_uart.v rtl/7seg/7seg.v rtl/7seg/BCDto7Seg.v rtl/7seg/BinToBCD.v
+VERILOG_FILES = rtl/Snake.v rtl/top.v  rtl/VGA/VgaSyncGen.v  rtl/UART/rx_uart.v rtl/7seg/7seg.v rtl/7seg/BCDto7Seg.v rtl/7seg/BinToBCD.v rtl/ram.v
 
 PCF_FILE = FPGA/blackice-mx.pcf
 
--- rtl/ram.v
+++ rtl/ram.v
@@ -1,4 +1,4 @@
-module sram #(parameter ADDR_WIDTH=10, DATA_WIDTH=2, DEPTH=640) (
+module sram #(parameter ADDR_WIDTH=9, DATA_WIDTH=1, DEPTH=320, INIT = 1) (
     input wire i_clk,
     input wire [ADDR_WIDTH-1:0] i_addr, 
     input wire i_write,
@@ -18,4 +18,36 @@ module sram #(parameter ADDR_WIDTH=10, DATA_WIDTH=2, DEPTH=640) (
             o_data <= memory_array[i_addr];
         end     
     end
+
+
+   
+
+    //-------------------------------------------
+    //             RAM initialization
+    //-------------------------------------------
+    integer i;
+    initial begin
+    	if (INIT) begin
+    	
+		for (i = 0; i < 64; i = i + 1)
+			memory_array[i] = 0;
+		for (i = 64; i < 128; i = i + 1)
+			if (i%8 <= 1 ) memory_array[i] = 1;
+		for (i = 128; i < 192; i = i + 1)
+			if (i%8 <= 3) memory_array[i] = 1;
+		for (i = 192; i < 256; i = i + 1)
+			if (i%8 <= 5) memory_array[i] = 1;
+		for (i = 256; i < 320; i = i + 1)
+			memory_array[i] = 1;	
+    	end 
+        else begin
+		for (i = 0; i < DEPTH; i = i + 1)
+			memory_array[i] = 0;
+		for (i = 80*25+20; i <= 80*25+40; i = i + 1)
+			memory_array[i] = 4;
+		//memory_array[80*22+5] = 2;
+		
+        end
+    end
+
 endmodule
